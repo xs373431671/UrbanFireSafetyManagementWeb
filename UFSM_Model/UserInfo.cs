3840e5afa71e2c37d4c6b5c0c4ad5ebb9194a4b4
Song Xue <44465946+xs373431671@users.noreply.github.com>
2018-10-27 13:59:07 +0800

    testGitHub3

--- UFSM_Model/UserInfo.cs
+++ UFSM_Model/UserInfo.cs
@@ -17,7 +17,6 @@ namespace UFSM_Model
         public string Password { get; set; }
         public string Email { get; set; }
         public string Telephone { get; set; }        
-        public string TestGitHub { get; set; }
-        public string TestGitHub2 { get; set; }
+        public string TestGitHub2{get;set;}
     }
 }
