d771ee623ebaf7b3134e425763a48c4e33c27fc3
xs373431671 <373431671@qq.com>
2018-10-28 12:33:10 +0800

    UserInfo的数据层与业务层已基本写完

--- UFSM_BusinessLogicLayer/UserInfoService.cs
+++ UFSM_BusinessLogicLayer/UserInfoService.cs
@@ -16,19 +16,58 @@ namespace UFSM_BusinessLogicLayer
         private static List<UserInfo> userList = new UserInfoDal().GetUserInfoList();
 
 
-        //注册UserInfo，返回成功或失败字符串
-        public string AddUserInfo(UserInfo user)
+        /// <summary>
+        /// 向数据库注册UserInfo，返回一个bool。
+        /// </summary>
+        /// <param name="user">注册的UserInfo对象</param>
+        /// <param name="returnMessage">返回注册信息字符串：如注册成功、失败</param>
+        /// <returns></returns>
+        public bool AddUserInfo(UserInfo user,out string returnMessage)
         {
+            foreach(UserInfo u in userList)  
+            {
+                if(user.Account==u.Account)
+                {
+                    returnMessage = "对不起，该账号名已被占用";
+                    return false;
+                }
+            }
+
             UserInfoDal uid = new UserInfoDal();
             int influenceRow = uid.AddUserInfo(user);
             if(influenceRow>0)
             {
                 userList = new UserInfoDal().GetUserInfoList();
-                return "注册成功";
+                returnMessage = "注册成功，请返回登录";
+                return true;
             }
             else
             {
-                return "注册失败";
+                returnMessage = "数据库异常，注册失败，请稍后重试";
+                return false;
+            }
+        }
+
+
+        /// <summary>
+        /// 检查登录信息，若登录成功，返回UserInfo对象，并out登录信息
+        /// </summary>
+        /// <param name="user">要登录的UserInfo对象</param>
+        /// <param name="returnMessage">登录成功或失败的字符串</param>
+        /// <returns></returns>
+        public UserInfo CheckUserInfo(UserInfo user, out string returnMessage)
+        {
+            UserInfoDal uid = new UserInfoDal();
+            UserInfo sineInUser= uid.CheckUserInfo(user.Account, user.Password, userList);
+            if(sineInUser==null)
+            {
+                returnMessage = "账号或用户名错误！";
+                return null; 
+            }
+            else
+            {
+                returnMessage = "登录成功";
+                return sineInUser;
             }
         }
     }
